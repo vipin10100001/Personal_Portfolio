3f6d3aa2c9c36340979a58f2879cb0dcbd8d6038
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2024-02-15 19:35:13 +0530

    removed some components in navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -38,15 +38,7 @@ const Navbar = () => {
         </div>
 
         <div className="flex flex-row gap-5">
-          {Socials.map((social) => (
-            <Image
-              src={social.src}
-              alt={social.name}
-              key={social.name}
-              width={24}
-              height={24}
-            />
-          ))}
+          
         </div>
       </div>
     </div>
