498528a374c7baf3496de0088727907c41430129
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2024-02-28 10:26:38 +0530

    updated component

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,41 +1,45 @@
 import React from "react";
-import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
+    <div className="flex flex-col items-center justify-center py-20" id="projects">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
+
+        Check on my Github for more projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/crypto.png"
-          title="Crypto-Currency Finance Tracker"
-          description="A Website that displays live crypto currency details using React.Js and implementation of CoinGecko API"
-        />
-        <ProjectCard
-          src="/ministudymate.png"
-          title="MiniStudyMate"
-          description=" Web based Study Material Retrieval System • React, JavaScript, npm, SerpAPI, Firebase"          
-        />
-        <ProjectCard
-          src="/expense.png"
-          title="Expense Tracker"
-          description="A react-Web App that allows user to Track Expenses."
-        />
-         <ProjectCard
-          src="/spotify.png"
-          title="Spotify-Artist-searcher"
-          description="A React based Web App that displays Artist details using Spotify API"
-        />
-        <ProjectCard
-          src="/pokedex.png"
-          title="Pokedex"
-          description="A React based Web App that displays Pokemon details using PokeAPI"
-        />
+        <div className="max-w-md rounded overflow-hidden shadow-lg">
+          <img className="w-full" src="/crypto.png" alt="Crypto-Currency Finance Tracker" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Crypto-Currency Finance Tracker</div>
+            <p className="text-white text-base">A Website that displays live crypto currency details using React.Js and implementation of CoinGecko API</p>
+           
+          </div>
+        </div>
+        <div className="max-w-md rounded overflow-hidden shadow-lg">
+          <img className="w-full" src="/ministudymate.png" alt="MiniStudyMate" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">MiniStudyMate</div>
+            <p className="text-white text-base">Web based Study Material Retrieval System • React, JavaScript, npm, SerpAPI, Firebase</p>
+            
+          </div>
+        </div>
+        <div className="max-w-md rounded overflow-hidden shadow-lg">
+          <img className="w-full" src="/expense.png" alt="Expense Tracker" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Expense Tracker</div>
+            <p className="text-white text-base">A react-Web App that allows user to Track Expenses.</p>
+          
+          </div>
+        </div>
+        <div className="max-w-md rounded overflow-hidden shadow-lg">
+          <img className="w-full" src="/spotify.png" alt="Spotify-Artist-searcher" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Spotify-Artist-searcher</div>
+            <p className="text-white text-base">A React based Web App that displays Artist details using Spotify API</p>
+          </div>
+        </div>
       </div>
     </div>
   );
