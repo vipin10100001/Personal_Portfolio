284bd9849b9f1cf5591c3dd2d09c2f7626020196
vipin10100001 <75900544+vipin10100001@users.noreply.github.com>
2024-04-08 21:50:07 +0530

    update in navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           </span>
         </a>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
+        <div className="w-[700px] h-full flex flex-row items-center justify-between md:mr-20">
           <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
